17de777c892d41348141856b57b293c8a3d6a49a
Peter Hamilton <peter.allen.hamilton@gmail.com>
2017-12-14 16:06:30 -0500

    Adding pep8 and bandit test infrastructure
    This change adds support for pep8/flake8 and bandit testing. Both
    flake8 and bandit are added as test requirements, while tox.ini
    and setup.py are updated to include basic information needed to
    install SLUG and run the tox framework. Additional test related
    output files are added to .gitignore.

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Copyright 2017, The Johns Hopkins University/Applied Physics Laboratory
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import setuptools
+
+
+setuptools.setup(
+    name="SLUG",
+    version="0.1.0",
+    description="A Simple, Lightweight User Group Service.",
+    keywords="authentication",
+    author="Peter Hamilton",
+    author_email="peter.allen.hamilton@gmail.com",
+    url="",
+    license="Apache License, Version 2.0",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: CherryPy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
