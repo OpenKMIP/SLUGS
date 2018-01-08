235c63e2f392211298106fabb79e675050476a06
Peter Hamilton <peter.allen.hamilton@gmail.com>
2018-01-07 20:26:57 -0500

    Adding empty unit test modules
    This change fleshes out the basic unit test infrastructure, adding
    test suites for all plugins and controllers. The requirements, tox
    setup, and .gitignore are updated to reflect these changes. To run
    the tests:
    
    $ tox -r
    
    All test methods are currently skipped, and pep8 will complain
    about mock not being used. Aside from that, tests pass.

---
+++ slug/tests/unit/test_controllers.py
@@ -0,0 +1,77 @@
+# Copyright 2017, The Johns Hopkins University/Applied Physics Laboratory
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import testtools
+
+
+class TestMainController(testtools.TestCase):
+
+    def setUp(self):
+        super(TestMainController, self).setUp()
+
+    def test_init(self):
+        self.skip('')
+
+    def test_update(self):
+        self.skip('')
+
+    def test_cp_dispatch_a_bunch(self):
+        self.skip('')
+
+    def test_index(self):
+        self.skip('')
+
+
+class TestUsersController(testtools.TestCase):
+
+    def setUp(self):
+        super(TestUsersController, self).setUp()
+
+    def test_init(self):
+        self.skip('')
+
+    def test_update_with_none(self):
+        self.skip('')
+
+    def test_update_with_data(self):
+        self.skip('')
+
+    def test_list(self):
+        self.skip('')
+
+    def test_index_a_bunch(self):
+        self.skip('')
+
+
+class TestGroupsController(testtools.TestCase):
+
+    def setUp(self):
+        super(TestGroupsController, self).setUp()
+
+    def test_init(self):
+        self.skip('')
+
+    def test_update_with_none(self):
+        self.skip('')
+
+    def test_update_with_data(self):
+        self.skip('')
+
+    def test_list(self):
+        self.skip('')
+
+    def test_index_a_bunch(self):
+        self.skip('')
