862c79245ce9cb4b7316668df12787935fdd4573
Peter Hamilton <peter.allen.hamilton@gmail.com>
2018-01-29 01:06:52 -0500

    Update Sphinx configuration to remove _static reference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,7 +100,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -148,7 +148,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'slugs.tex', u'slugs Documentation',
+    (master_doc, 'slugs.tex', u'SLUGS Documentation',
      u'Peter Hamilton', 'manual'),
 ]
 
@@ -158,7 +158,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'slugs', u'slugs Documentation',
+    (master_doc, 'slugs', u'SLUGS Documentation',
      [author], 1)
 ]
 
@@ -169,7 +169,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'slugs', u'slugs Documentation',
+    (master_doc, 'slugs', u'SLUGS Documentation',
      author, 'slugs', 'One line description of project.',
      'Miscellaneous'),
 ]
